3b92707217f368e4e2e39bb247305b4b963c59cc
MardinliG <guillaume.mardinli@ynov.com>
2025-03-05 09:02:33 +0100

    add

--- initdb.js
+++ initdb.js
@@ -10,7 +10,7 @@ const { Pool } = pkg;
 
 // Variables d'environnement pour PostgreSQL
 const DB_USER = process.env.DB_USER || 'postgres';
-const DB_PASSWORD = process.env.DB_PASSWORD || 'sql';
+const DB_PASSWORD = process.env.DB_PASSWORD || 'root';
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || 5432;
 const DB_DATABASE = process.env.DB_DATABASE || 'dbcocktail';
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // Configuration de la connexion à PostgreSQL
 const pool = new Pool({
     user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'sql',
+    password: process.env.DB_PASSWORD || 'root',
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
     database: process.env.DB_DATABASE || 'dbcocktail',
